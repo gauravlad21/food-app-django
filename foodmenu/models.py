d50b79931473567433af0a2e6a65f697fe734910
gauravlad21 <gauravlad21@gmail.com>
2022-12-27 14:29:23 +0530

    added image url

--- foodmenu/models.py
+++ foodmenu/models.py
@@ -5,6 +5,9 @@ class Food(models.Model):
     title = models.CharField(max_length=32)
     desc = models.CharField(max_length=512)
     price = models.IntegerField()
+    image = models.CharField(max_length=512,
+                             default='https://png.pngtree.com/png-clipart/20190612/original/pngtree-black-and-'
+                                     'white-variety-of-food-png-image_3319164.jpg')
 
     def __str__(self):
         return self.title
