c0bf7416f819338097d60fabd893fc9a8d780f70
gauravlad21 <gauravlad21@gmail.com>
2022-12-29 10:59:55 +0530

    form

--- foodmenu/urls.py
+++ foodmenu/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, food_details
+from .views import index, food_details, create_food, update_food, delete_food
 
 app_name = 'food'
 urlpatterns = [
     path('food/', index, name='index'),
     path('food/<int:pk>/', food_details, name='detail'),
+    path('food/add/', create_food, name='add-food'),
+    path('food/update/<int:pk>/', update_food, name='update-food')
+    path('food/update/<int:pk>/', delete_food, name='delete-food')
 ]
--- foodmenu/views.py
+++ foodmenu/views.py
@@ -1,6 +1,6 @@
 from .models import Food
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FoodForm
 
 
 def index(request):
@@ -17,3 +17,28 @@ def food_details(request, pk):
         "item": item,
     }
     return render(request, 'foodmenu/detail.html', context)
+
+
+def create_food(request):
+    form = FoodForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("food:index")
+    return render(request, 'foodmenu/food-form.html', {'form': form})
+
+
+def update_food(request, pk):
+    item = Food.objects.get(pk=pk)
+    form = FoodForm(request.POST or None, instance=item)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request, 'foodmenu/food-form.html', {'form': form})
+
+
+def delete_food(request, pk):
+    item = Food.objects.get(pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    return render(request, 'foodmenu/food-delete.html', {'form': form})
